b595b8c5aff50946d6a494b110a5d02705648ce3
IgiranezaMichel <igiramichael@gmail.com>
2023-12-21 10:07:23 +0200

    first commit

---
+++ family-reunification/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { VisitorNavigationBar } from './Component/VisitorNavigationBar'
+
+function App() {
+
+
+  return (
+    <>
+      <VisitorNavigationBar/>
+    </>
+  )
+}
+
+export default App
